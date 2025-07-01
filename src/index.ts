c11c11556b73def824f8e5609f02b7b29a9b9778
Mani <deepanshukumar1542004@gmail.com>
2025-07-02 03:16:37 +0530

    update url

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ const server = http.createServer((req, res) => {
 
 setupWebSocket(server);
 
-server.listen(8080, () => {
-  console.log("Server running on http://localhost:8080");
+const PORT = Number(process.env.PORT) || 8080;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
